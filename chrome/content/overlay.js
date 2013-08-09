ef5544c9aa5cda20f9cec8b46c608b5d3971e215
Jonathan GAULUPEAU <jo.gaulupeau@gmail.com>
2013-08-09 23:20:56 +0200

    Init

---
+++ chrome/content/overlay.js
@@ -0,0 +1,53 @@
+if (typeof poche == "undefined") {
+	var poche = {
+		_prefs: Components.classes["@mozilla.org/preferences-service;1"]
+			.getService(Components.interfaces.nsIPrefService).getBranch("extensions.poche@gaulupeau.fr."),
+
+		installButton: function (toolbarId, id, afterId) {
+				if (!document.getElementById(id)) {
+						var toolbar = document.getElementById(toolbarId);
+
+						// If no afterId is given, then append the item to the toolbar
+						var before = null;
+						if (afterId) {
+								var elem = document.getElementById(afterId);
+								if (elem && elem.parentNode == toolbar)
+										before = elem.nextElementSibling;
+						}
+
+						toolbar.insertItem(id, before);
+						toolbar.setAttribute("currentset", toolbar.currentSet);
+						document.persist(toolbar.id, "currentset");
+
+						if (toolbarId == "addon-bar")
+								toolbar.collapsed = false;
+				}
+		},
+
+		post: function(event) {
+			var url = content.document.location.href;
+
+			var width = 600;
+			var height = 390;
+			var left = window.mozInnerScreenX + (window.innerWidth - width) / 2;
+			var top = window.mozInnerScreenY + (window.innerHeight - height) / 2;
+			window.open(this._prefs.getCharPref("url") + "?action=add&url=" + btoa(url),
+									"", "height=" + height + ",width=" + width + ",top=" + top + ",left=" + left + ",toolbar=no,menubar=no,scrollbars=no,status=no,dialog,modal");
+			event.stopPropagation();
+		},
+
+		open: function(event) {
+			gBrowser.selectedTab = gBrowser.addTab(this._prefs.getCharPref("url"));
+			event.stopPropagation();
+		}
+	};
+
+	window.addEventListener("load", function() {
+		Application.getExtensions(function(extensions) {
+			var extension = extensions.get("poche@gaulupeau.fr");
+			if (extension.firstRun) {
+				poche.installButton("nav-bar", "poche-toolbar-button");
+			}
+		});
+	}, false);
+}
