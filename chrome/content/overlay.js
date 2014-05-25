e5060c33658ed61480f1d2f86203fa51891b327f
Paul Park <paul-git@idealworldinc.com>
2014-05-25 16:37:35 -0400

    Add api.php support.  Fall back to web if not configured or if there is an error.

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,7 +1,23 @@
-if (typeof wallabag == "undefined") {
-	var wallabag = {
+if (typeof wallabagAPI == "undefined") {
+	var wallabagAPI = {
 		_prefs: Components.classes["@mozilla.org/preferences-service;1"]
-			.getService(Components.interfaces.nsIPrefService).getBranch("extensions.wallabag@gaulupeau.fr."),
+			.getService(Components.interfaces.nsIPrefService).getBranch("extensions.wallabag.api@idealworldinc.com."),
+
+		showNotification: function(nText) {
+			var nBox = gBrowser.getNotificationBox();
+			var nInstance = nBox.getNotificationWithValue('wallabag-api');
+			if (nInstance) {
+			    nInstance.label = nText;
+			} else {
+			    nInstance = nBox.appendNotification(nText, 'wallabag-api',
+						    'chrome://wallabag/skin/icon.png',
+						    nBox.PRIORITY_INFO_HIGH,
+						    null);
+			}
+			setTimeout(function() {
+			    nBox.removeNotification(nInstance);
+			}, 5000);
+		},
 
 		installButton: function (toolbarId, id, afterId) {
 				if (!document.getElementById(id)) {
@@ -24,7 +40,7 @@ if (typeof wallabag == "undefined") {
 				}
 		},
 
-		post: function(event) {
+		postWindow: function(event) {
 			var url = content.document.location.href;
 
 			var width = 600;
@@ -32,8 +48,58 @@ if (typeof wallabag == "undefined") {
 			var left = window.mozInnerScreenX + (window.innerWidth - width) / 2;
 			var top = window.mozInnerScreenY + (window.innerHeight - height) / 2;
 			window.open(this._prefs.getCharPref("url") + "?action=add&url=" + btoa(url),
-									"", "height=" + height + ",width=" + width + ",top=" + top + ",left=" + left + ",toolbar=no,menubar=no,scrollbars=no,status=no,dialog,modal");
-			event.stopPropagation();
+				    "", "height=" + height + ",width=" + width + ",top=" + top + ",left=" + left + ",toolbar=no,menubar=no,scrollbars=no,status=no,dialog,modal");
+		},
+
+		post: function(event) {
+		    let un = this._prefs.prefHasUserValue('username') ? 
+			this._prefs.getCharPref('username') : null;
+		    let pass = this._prefs.prefHasUserValue('password') ? 
+			this._prefs.getCharPref('password') : null;
+		    let pUrl = this._prefs.prefHasUserValue('url') ? 
+			this._prefs.getCharPref('url') : null;
+		    if (pUrl && pUrl.length) {
+			if (un && un.length && pass && pass.length) {
+			    var url = content.document.location.href;
+			    var postXHR = new XMLHttpRequest();
+			    postXHR.addEventListener('load', function (xe) {
+				if (postXHR.status == 200) {
+				    try {
+					let rObj = 
+					    JSON.parse(postXHR.responseText);
+					if (rObj.status == 0) {
+					    wallabagAPI.showNotification('Added link ' + rObj.message);
+					} else {
+					    wallabagAPI.showNotification('Problem adding link ' + rObj.message);
+					}
+				    }
+				    catch (te) {
+					wallabagAPI.showNotification('Response error ' + te.message);
+				    }
+				}
+			    });
+			    postXHR.addEventListener('error', function (xe) {
+				wallabagAPI.showNotification('Error posting URL: ' + xe.message + ', tryinv via web.');
+				wallabagAPI.postWindow(event);
+			    });
+			    postXHR.addEventListener('abort', function (xe) {
+				wallabagAPI.showNotification('URL post aborted.');
+			    });
+			    postXHR.open('POST', pUrl + 'api.php', true);
+			    var formData = new FormData();
+			    formData.append('action', 'add');
+			    formData.append('login', un);
+			    formData.append('password', pass);
+			    formData.append('url', btoa(url));
+			    postXHR.send(formData);
+			} else {
+			    wallabagAPI.showNotification('No username or password configured, trying via web.');
+			    wallabagAPI.postWindow(event);
+			}
+		    } else {
+			wallabagAPI.showNotification('Need to configure wallabag extension.');
+		    }
+		    event.stopPropagation();
 		},
 
 		open: function(event) {
@@ -44,9 +110,9 @@ if (typeof wallabag == "undefined") {
 
 	window.addEventListener("load", function() {
 		Application.getExtensions(function(extensions) {
-			var extension = extensions.get("wallabag@gaulupeau.fr");
+			var extension = extensions.get("wallabag.api@idealworldinc.com");
 			if (extension.firstRun) {
-				wallabag.installButton("nav-bar", "wallabag-toolbar-button");
+				wallabagAPI.installButton("nav-bar", "wallabag-toolbar-button");
 			}
 		});
 	}, false);
